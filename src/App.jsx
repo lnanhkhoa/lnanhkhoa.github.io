7e4b84cda842ab3fc1135b373424b98dcf5ea164
Khoa Le <lnanhkhoa303@gmail.com>
2025-08-05 18:57:01 +0700

    perf: implement lazy loading for page components with suspense fallback

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
-import Home from './pages/Home'
-import Blog from './pages/Blog'
-import BlogPost from './pages/BlogPost'
+const Home = React.lazy(() => import('./pages/Home'));
+const Blog = React.lazy(() => import('./pages/Blog'));
+const BlogPost = React.lazy(() => import('./pages/BlogPost'));
 
 function App() {
   return (
     <Router>
       <div className="min-h-screen bg-background">
         <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/blog/:id" element={<BlogPost />} />
-        </Routes>
+        <React.Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/blog/:id" element={<BlogPost />} />
+          </Routes>
+        </React.Suspense>
       </div>
     </Router>
   )
